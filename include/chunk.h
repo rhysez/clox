9404179d0d7da9ba6e6b7e12291ced6497037a04
Rhys Hodgson <121772484+rhysez@users.noreply.github.com>
2025-01-25 19:01:48 +0000

    write writeChunk function and header declaration

---
+++ include/chunk.h
@@ -0,0 +1,23 @@
+#ifndef clox_chunk_h
+#define clox_chunk_h
+
+#include "common.h"
+
+// A 'chunk' refers to a sequence of bytecode.
+
+// Functions to be executed from instructions.
+typedef enum {
+    OP_RETURN, // Returns from the current function.
+} Opcode;
+
+// Bytecode instructions
+typedef struct {
+    int count; // The amount of allocated elements in use.
+    int capacity; // The amount of elements we have allocated to the array.
+    uint8_t* code; // A pointer to an array of unsigned 8-bit integers; AKA the opcode.
+} Chunk;
+
+void initChunk(Chunk* chunk);
+void writeChunk(Chunk* chunk, uint8_t byte);
+
+#endif
